8016334f95da0d2c804a3b2117734309177ba074
cfajitas <cfajitas@gmail.com>
2016-04-07 20:14:18 +0000

    Setting up

---
+++ Astar.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello Cloud9 A* Code Here \n";
+    return 0;
+}
---
+++ Genetic.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello Cloud9 Genetic Code Here \n";
+    return 0;
+}
